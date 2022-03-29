12bdde54aeeb40560e306097637aae439bb0eacf
David Emilio Iriondo Palacios <diriondo@emeal.nttdata.com>
2022-03-29 14:45:34 +0200

    First version

---
+++ GitProjectExample/src/App.java
@@ -0,0 +1,8 @@
+/**
+ * @author David Iriondo Palacios
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, this is an example of git repository!");
+    }
+}
