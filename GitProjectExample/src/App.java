28fdd41a600817e393a9f18791252c71092e1117
David Emilio Iriondo Palacios <diriondo@emeal.nttdata.com>
2022-03-29 14:46:42 +0200

    Second version

--- GitProjectExample/src/App.java
+++ GitProjectExample/src/App.java
@@ -3,6 +3,6 @@
  */
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, this is an example of git repository!");
+        System.out.println("Hey this is another example of git repository!");
     }
 }
